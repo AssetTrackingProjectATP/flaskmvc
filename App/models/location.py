fce280ef5cead4794fc218fd01ac402d3b4eedf4
Enzo-TTD <lorenzogouldd@gmail.com>
2025-02-22 00:25:25 +0000

    added floor feild in location model, added location controller

--- App/models/location.py
+++ App/models/location.py
@@ -3,13 +3,16 @@ from App.database import db
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     room_name = db.Column(db.String(30), nullable=False)
+    floor_name = db.Column(db.String(30), nullable=False)
     building_name = db.Column(db.String(30), nullable=False)
+    
 
-    #this allows same names of rooms to be used in different buildings, for example
-    #Building 1 room 1, Building 2 room 2 is allowed but not Building 1 room 1, Building 1 room 1
-    __table_args__ = (db.UniqueConstraint('room_name', 'building_name', name='uq_room_building'),)
+    #allows same room name to be used on different floors and
+    #but prevent dupilcate combinations of buildings, floor and room
+    #location with the same room, floor and building of a other location in the db will be rejected
+    __table_args__ = (db.UniqueConstraint('room_name', 'building_name', 'floor_name', name='uq_room_building_floor'),)
 
-    def __init__(self, room_name, building_name):
+    def __init__(self, room_name, floor_name, building_name):
         self.room_name = room_name
         self.building_name = building_name
 
@@ -17,6 +20,7 @@ class Location(db.Model):
         return{
             'id': self.id,
             'room': self.room_name,
+            'floor': self.floor_name,
             'building': self.building_name
 
         }
