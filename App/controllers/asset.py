51185d7c76c89bc8b8c4df634ab589d61b9f0934
PhineasMun <123189426+PhineasMun@users.noreply.github.com>
2025-02-24 02:53:07 +0000

    asset

--- App/controllers/asset.py
+++ App/controllers/asset.py
@@ -2,6 +2,16 @@ from App.models import Asset
 from App.database import db 
 
 
+def add_asset(id, name, item_class, last_update, serial_number, change_log):
+    newAsset = Asset(id, name, item_class, last_update, serial_number, change_log)
+    try:
+        db.session.add(newAsset)
+        db.session.commit()
+        return newAsset
+    except:
+        db.session.rollback()
+        return None
+
 def get_asset(id):
     return Asset.query.filter_by(id=id).first()
 
@@ -15,6 +25,10 @@ def get_all_assets_json():
     assets = [asset.get_json() for asset in assets]
     return assets
 
-
+def update_condition(id, condition):
+    asset = get_asset(id)
+    asset.condition = condition
+    
+    return asset
     
     
