fecd298ab5103d2d4dee5fb61292f2511f58ce8a
PhineasMun <123189426+PhineasMun@users.noreply.github.com>
2025-03-22 03:20:06 +0000

    add csv controller

--- App/controllers/asset.py
+++ App/controllers/asset.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from App.models import Asset 
+import os, csv
 from App.models.asset import *
 from App.controllers.assignee import *
 from App.controllers.scanevent import add_scan_event
@@ -59,6 +60,31 @@ def set_status(id):
         new_asset.status = "Misplaced"
         
     return new_asset
+    
+    
+def upload_csv(self):
+    with open('CSVsample.csv') as file:
+     reader = csv.DictReader(file)
+     for row in reader:
+         
+
+      new_asset = Asset(description=row['item'],
+                            id=row['Asset Tag'],
+                            model= row['Model'],
+                            brand=row['Brand'],
+                            serial_number=row['Serial Number'],
+                            room_id=row['Location'],
+                            last_located=row['Location'],
+                            status=row['Condition'],
+                            assignee_id=row['Assignee'],
+                            last_update=db.func.current_timestamp(),
+                            notes=null
+                            
+      )
+    
+     db.session.add(new_asset)
+     db.session.commit()
+        
 
 
 def update_asset_location(asset_id, new_location, user_id=None):
